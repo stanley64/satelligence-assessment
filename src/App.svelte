d794b630652ed15fce699840f1b87bd390030ddb
Stanley <stanleyh64@gmail.com>
2024-09-21 14:23:20 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Map } from '@beyonk/svelte-mapbox'
+	import Instructions from './Instructions.svelte'
+	
+  let mapComponent
+
+	function onReady() {
+    mapComponent.flyTo({center:[5,52]}) 
+	}
+</script>
+
+<Instructions/>
+
+<Map accessToken="pk.eyJ1Ijoic2F0ZWxsaWdlbmNlLXN0YWdpbmciLCJhIjoiY2w2cWtxaGNtMGJlYjNkdGNsMXI4MnpiYSJ9.LEONl2580jXyWbjJE7iMaQ"
+	style="mapbox://styles/mapbox/light-v11"
+	zoom=6
+	version="v2.12.0"
+  bind:this={mapComponent} 
+  on:ready={onReady}
+>
+</Map>
