cc5f72b84fcbc0a087b8087d1ea71815342d9f3b
Stanley <stanleyh64@gmail.com>
2024-09-21 15:16:13 +0200

    Remove Instructions and add initial layout for EventDetails

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { Map } from '@beyonk/svelte-mapbox';
-  import Instructions from './Instructions.svelte';
 
   let mapComponent;
 
@@ -8,8 +7,6 @@
     mapComponent.flyTo({ center: [5, 52] });
   }
 </script>
-
-<Instructions />
 
 <Map
   accessToken="pk.eyJ1Ijoic2F0ZWxsaWdlbmNlLXN0YWdpbmciLCJhIjoiY2w2cWtxaGNtMGJlYjNkdGNsMXI4MnpiYSJ9.LEONl2580jXyWbjJE7iMaQ"
---
+++ src/EventDetails.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  const onClose = (): void => {
+    dispatch('close');
+  };
+
+  export let event: any;
+</script>
+
+<div>
+  <div class="header-container">
+    <div class="title-container">
+      <span>Event Reference: </span>
+      <span>Deforestation Event</span>
+    </div>
+    <button type="button" on:click={onClose}>X</button>
+  </div>
+  <div class="share-container">
+    <span>Lat: 123, Long: 123</span>
+    <div class="share-buttons-container">
+      <button type="button">SHP</button>
+      <button type="button">Copy URL</button>
+    </div>
+  </div>
+  <table>
+    <tr>
+      <td>Last Detected:</td>
+      <td>1</td>
+    </tr>
+    <tr>
+      <td>Region:</td>
+      <td>2</td>
+    </tr>
+    <tr>
+      <td>Total Size:</td>
+      <td>3</td>
+    </tr>
+    <tr>
+      <td>Forest Type:</td>
+      <td>4</td>
+    </tr>
+    <tr>
+      <td>Noncompliance:</td>
+      <td>5</td>
+    </tr>
+  </table>
+</div>
+
+<style>
+  .header-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .title-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .share-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .share-buttons-container {
+    display: flex;
+  }
+</style>
